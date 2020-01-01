c21c39e35a17a31457c2108ceb718d0a1e69109d
Chidimma Okafor <chidimma.okafor.c@gmail.com>
2020-01-01 15:51:26 +0100

    Setup formatting for amount and created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
-
     <Link to={`/edit/${id}`}>
       <h4>{description}</h4>
     </Link>
-    <p>Amount Spent: {amount}</p>
-    <p>Created At: {createdAt}</p>
+    <p>Amount Spent: {`#${numeral(amount / 100).format("0,0.00")}`}</p>
+    <p>Created At: {moment(createdAt).format("MMMM Do, YYYY")}</p>
   </div>
 );
 
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ExpenseListItem from '../../components/ExpenseListItem';
-import expenses from '../fixtures/expenses';
+import React from "react";
+import { shallow } from "enzyme";
+import ExpenseListItem from "../../components/ExpenseListItem";
+import expenses from "../fixtures/expenses";
 
 const [expense] = expenses;
-describe('Rendering <ExpenseListItem/>', () => {
-  it('Render the <ExpenseListItem/> with expense as props', () => {
+describe("Rendering <ExpenseListItem/>", () => {
+  it("Render the <ExpenseListItem/> with expense as props", () => {
     const wrapper = shallow(<ExpenseListItem {...expense} />);
     expect(wrapper).toMatchSnapshot();
   });
-}); 
+});

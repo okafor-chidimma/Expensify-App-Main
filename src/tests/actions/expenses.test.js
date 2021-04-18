4193dae22ac2880bacf6264cfbf0180d8461cc76
okafor-chidimma <chidimma.okafor.c@gmail.com>
2021-04-18 16:54:06 -0700

    Update expenses.test.js

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -132,16 +132,43 @@ describe('the Expense Action Generators', () => {
     // step 6 dispatch all the actions
     // this dispatch is just like the one our component is doing
     // to assert stuffs about what it returns, we have to make sure the promise returns stuff in startAddExpense() in expense.js.
-    // startAddExpense() returns a promise, so we can chain a then() and now assert inside the then
+    
+    // startAddExpense() action generator returns a promise,how does it return a promise since we asked it to return a function?
+    //the returned function is not for us, we do not have access to it rather it is called internally by redux, and when called the internal function starts executing.
+    //it reaches the point in the internal function where we have explicitly told it to return a promise, so it returns the promise, but since the promise has been called
+    //it must either resolve or reject. this means that the promise that is returned, at the point of the return is still pending but before our then block in this test file can
+    //run, it must have resolved
+    //in summary what startAddExpense() really returns is that promise in its pending state to allow us continue chaining and not the inner function
+    //remember that the returned promised although pending must resolve later in the future even if we did not later use do any promise chaining.
+    
+    //RECAP
+    //1. WHAT STARTADDEXPENSE() REALLY RETURNS THAT WE HAVE ACCESS TO IS A PROMISE IN ITS PENDING STATE.
+    //2. WHY WE RETURNED THE PROMISE IS TO ALLOW US CONTINUE PROMISE CHAINING i.e ADDING OUR OWN THEN BLOCK TO THE RETURNED PROMISE
+    //3. OUR THEN() ONLY RUNS AFTER THE RETURNED PROMISE HAS BEEN RESOLVED
+    
+    
+    //so we can chain a then() and now assert inside the then
     store
-      .dispatch(startAddExpense(expenseData))
+      .dispatch(startAddExpense(expenseData))//returns our pending promise
       .then(() => {
+      //before this then block can run, my pending promise must resolve and also run the then block it was returned with
+      
+        //why use done()?
         // since this then is an async function, without done(), jest runs the test suite without waiting for this to run but since we have done() as an arg, we call it here to force jest to wait for this async function to run before completing the test suite
-
-        // to get the dispatched actions so we can make sure the store received it and also make sure what we sent to the db is sent as well
+        
+      
+        //why must we call store.getActions() and why must it be called from inside of the then() block as seen below?
+        //A. to get the dispatched actions so we can make sure the store received it and also make sure what we sent to the db is sent as well
+        
+        //B i. the main purpose of the test is to ensure that the right action got sent(dispatched) to the store by redux
+        //B ii. we can only get that action after the asynchronous request has finsihed running. if we had taken store.getAction() outside of this then(), it will run but the actions array
+        //it will return may not have the latest or most recent actions in it because our async request may not have finished by that time. so the only to ensure that we get our actions after
+        //our async req has finished, is by putting it in the then() success block
+        
+        
         const actions = store.getActions();
         console.log(actions, 'hi actions');
-        // since it is only the internally generated dispatch that actually gets ent to the store, we will have only one action
+        // since it is only the internally generated dispatch that actually gets sent to the store, we will have only one action
         // store.getActions() returns an array
         expect(actions[0]).toEqual({
           type: 'ADD_EXPENSE',

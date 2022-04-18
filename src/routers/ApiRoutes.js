34f4cee4b847a5df2dbe28584c79adc80dac3506
okafor-chidimma <chidimma.okafor.c@gmail.com>
2022-04-18 23:01:45 +0100

    Update ApiRoutes.js

--- src/routers/ApiRoutes.js
+++ src/routers/ApiRoutes.js
@@ -15,13 +15,20 @@ export const history = createBrowserHistory();
 const ApiRoutes = () => (
   // browserRouter needs to have only one direct child, hence the div
   /*
-    when we use browser router, react-router by default is creating browser history and is registering it with our new instances of Route
-
+    when we use browser router, react-router by default is creating browser history and is registering it with our new instances of Route and we can access
+    it via props
+    
+    but we want access to the history of the app outside of the browser rouuter component or routes registered in the browser router, i.e we are using it 
+    in the firebase Authentication in app.js, and we also want to keep our client side rendering(allowing the routing to be controlled by react-router-dom 
+    instead of the server), we would have to do what BrowserRouter was doing automatically manually
+    
+    
     we can do this manually, by doing the following
     1. install history npm package so we can have access to push,location etc
     2. Import into your project
     3. create an instance of it called history
     4. change BrowserRouter Instance to Router and pass in the history props
+    
   */
   // <BrowserRouter>
   //   <div>
